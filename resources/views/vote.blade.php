7bb3aa46a551337d09f42d0b60cf53a3a5da0628
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-04 18:40:57 +0200

    Fix condizione voto

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -8,7 +8,7 @@
     <section class="container" id="vote">
            <ul class="row row-cols-1 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5 text-center mt-3"> 
             @foreach ($movies as $movie)
-                <li class="card {{ ($movie->vote) ? 'success' : 'danger'}} gy-2">
+                <li class="card {{ ($movie->vote >= 6) ? 'success' : 'danger'}} gy-2">
                     <h1 class="mt-5 mb-5">{{ $movie->title }}</h1>
                     <div class="text-primary fs-4 py-3">{{ $movie->vote }}</div>
                 </li>
